1ce539767b993b0da28ec68d7ade046050f8b68f
Kirk Radish <kirk@kirkradish.com>
2018-06-09 22:09:14 +0000

    Build single-meeting template

--- single-meeting.php
+++ single-meeting.php
@@ -10,14 +10,17 @@
 
 require 'inc/bbg-functions-assemble.php';
 
-if ( have_posts() ) {
+if (have_posts()) {
 	the_post();
 
+	// GATHER FIELD DATA
 	$metaAuthor = get_the_author();
 	$metaKeywords = strip_tags(get_the_tag_list('',', ',''));
 	$ogTitle = get_the_title();
 	$ogDescription = get_the_excerpt();
+	$meetingTime = get_post_meta( get_the_ID(), 'board_meeting_time', true );
 
+	// SET PAGE TYPE VARS
 	$eventPageHeader = "Event";
 	$isBoardMeeting = false;
 	$isPressRelease = false;
@@ -29,7 +32,7 @@ if ( have_posts() ) {
 		$isPressRelease = true;
 	}
 
-	/**** CREATE OG:IMAGE *****/
+	// CREATE OG:IMAGE
 	$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'Full' );
 	$ogImage = $thumb['0'];
 	$socialImageID = get_post_meta( $post->ID, 'social_image',true );
@@ -38,7 +41,7 @@ if ( have_posts() ) {
 		$ogImage = $socialImage[0];
 	}
 
-	/**** CREATE $bannerAdjustStr *****/
+	// ADJUST BANNER IMAGE STYLE
 	$bannerPosition = get_post_meta( get_the_ID(), 'adjust_the_banner_image', true);
 	$bannerPositionCSS = get_field( 'adjust_the_banner_image_css', '', true);
 	$bannerAdjustStr = "";
@@ -47,8 +50,6 @@ if ( have_posts() ) {
 	} else if ($bannerPosition) {
 		$bannerAdjustStr = $bannerPosition;
 	}
-
-	$meetingTime = get_post_meta( get_the_ID(), 'board_meeting_time', true );
 
 	$today = new DateTime("now", new DateTimeZone('America/New_York'));
 	$todayStr = $today->format('Y-m-d H:i:s');
@@ -85,7 +86,7 @@ if ( have_posts() ) {
 	}
 	$meetingSpeakers = get_post_meta( get_the_ID(), 'board_meeting_speakers', true );
 
-	/*** CREATE EVENTBRITE IFRAME ****/
+	// CREATE EVENTBRITE IFRAME
 	$eventBriteButtonStr = "";
 	$eventbriteUrl = get_post_meta( get_the_ID(), 'board_meeting_eventbrite_url', true );
 	if ($eventbriteUrl && $eventbriteUrl != "" && !$isPressRelease) {
@@ -110,15 +111,12 @@ $listsInclude = get_field( 'sidebar_dropdown_include', '', true);
 
 include get_template_directory() . "/inc/shared_sidebar.php";
 
-get_header(); ?>
+get_header();
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php
-
+<main id="main" class="site-main" role="main">
+	<?php
+		while ( have_posts() ) : the_post(); 
 			$projectCategoryID = get_cat_id('Project');
 			$isProject = has_category($projectCategoryID);
 
@@ -126,144 +124,101 @@ get_header(); ?>
 			$featuredImageClass = " bbg__article--no-featured-image";
 
 			//the title/headline field, followed by the URL and the author's twitter handle
-			$twitterText = "";
-			$twitterText .= html_entity_decode( get_the_title() );
+			$twitterText  = html_entity_decode(get_the_title());
 			$twitterText .= " by @bbggov " . get_permalink();
-			$twitterURL = "//twitter.com/intent/tweet?text=" . rawurlencode( $twitterText );
-			$fbUrl = "//www.facebook.com/sharer/sharer.php?u=" . urlencode( get_permalink() );
+
+			$twitterURL = "//twitter.com/intent/tweet?text=" . rawurlencode($twitterText);
+			$fbUrl = "//www.facebook.com/sharer/sharer.php?u=" . urlencode(get_permalink());
 			$postDate = get_the_date();
+		?>
 
-			?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class( "bbg__article" ); ?>>
-
-				<?php
-					//If a featured video is set, include it.
-					//ELSE if a featured image is set, include it.
-					// $hideFeaturedImage = false;
-					// if ($videoUrl != "") {
-					// 	$video_data featured_video($videoUrl);
-					// 	$hideFeaturedImage = true;
-
-					// 	$video_markup  = '<iframe scrolling="no" src="';
-					// 	$video_markup .= 	$video_data['url'];
-					// 	$video_markup .= 	'" frameborder="0" allowfullscreen="" data-ratio="NaN" data-width="" data-height="" style="display: block; margin: 0px;">';
-					// 	$video_markup .= '</iframe>';
-					// 	echo 
-					// } elseif ( has_post_thumbnail() && ( $hideFeaturedImage != 1 ) ) {
-					// 	echo '<div class="usa-grid-full">';
-					// 	$featuredImageClass = "";
-					// 	$featuredImageCutline="";
-					// 	$thumbnail_image = get_posts(array('p' => get_post_thumbnail_id(get_the_ID()), 'post_type' => 'attachment'));
-					// 	if ($thumbnail_image && isset($thumbnail_image[0])) {
-					// 		$featuredImageCutline=$thumbnail_image[0]->post_excerpt;
-					// 	}
-					// 	echo '<div class="single-post-thumbnail clear bbg__article-header__thumbnail--large">';
-					// 	echo the_post_thumbnail( 'large-thumb' );
-
-					// 	if ( $featuredImageCutline != "" ) {
-					// 		echo '<div class="usa-grid">';
-					// 			echo "<div class='wp-caption-text'>$featuredImageCutline</div>";
-					// 		echo '</div> <!-- usa-grid -->';
-					// 	}
-
-					// 	echo '</div>';
-					// 	echo '</div> <!-- usa-grid-full -->';
-					// }
-
-				display_feature_media_type();
-				?><!-- .bbg__article-header__thumbnail -->
-
-
-
-				<div class="bbg__article__nav">
-				</div><!-- .bbg__article__nav -->
-
-
-				<div class="usa-grid">
-
-					<?php echo '<header class="entry-header bbg__article-header' . $featuredImageClass . '">'; ?>
-
-						<div class="bbg__event-title">
-
-							<h5 class="entry-category bbg__label"><a href="/news/events/"><?php echo $eventPageHeader; ?></a>: <?php echo $postDate; ?></h5><!-- .bbg__label -->
-							<?php the_title( '<h1 class="entry-title bbg__article-header__title">', '</h1>' ); ?>
-							<!-- .bbg__article-header__title -->
-
-						</div>
-					</header><!-- .bbg__article-header -->
-
-					<div class="bbg__article-sidebar--left">
-						<h3 class="bbg__sidebar-label bbg__contact-label">Share </h3>
-						<ul class="bbg__article-share">
-							<li class="bbg__article-share__link facebook">
-								<a href="<?php echo $fbUrl; ?>">
-									<span class="bbg__article-share__icon facebook"></span>
-								</a>
-							</li>
-							<li class="bbg__article-share__link twitter">
-								<a href="<?php echo $twitterURL; ?>">
-									<span class="bbg__article-share__icon twitter"></span>
-								</a>
-							</li>
-						</ul>
-					</div>
-
-					<div class="entry-content bbg__article-content">
-
-						<?php
-
+		<article id="post-<?php the_ID(); ?>" <?php post_class( "bbg__article" ); ?>>
+			<div class="outer-container">
+				<div class="page-post-main-content">
+					<?php
+						$post_header  = '<header>';
+						$post_header .= 	'<h3>' . get_the_title() . '</h3>';
+						$post_header .= '</header>';
+						echo $post_header;
+					?>
+				</div>
+			</div>
+			<div class="outer-container">
+				<div class="page-post-main-content">
+					<?php
+						// if () {
+							$post_featured_image  = '<div class="page-post-featured-image">';
+							$post_featured_image .= 	get_the_post_thumbnail();
+							$post_featured_image .= '</div>';
+							echo $post_featured_image;
+						// }
+					?>
+					<div class="bbg__article-content">
+					<?php
 						if (isset($_GET['success'])) {
-							echo '<div class="usa-alert usa-alert-success">
-									<div class="usa-alert-body">
-									<h3 class="usa-alert-heading">Submission Successful</h3>
-									<p class="usa-alert-text">Your comment was successfully submitted.</p>
-									</div>
-									</div><BR>';
+							$success_alert  = '<div class="usa-alert usa-alert-success">';
+							$success_alert .= 	'<div class="usa-alert-body">';
+							$success_alert .= 		'<h3 class="usa-alert-heading">Submission Successful</h3>';
+							$success_alert .= 		'<p class="usa-alert-text">Your comment was successfully submitted.</p>';
+							$success_alert .= 	'</div>';
+							$success_alert .= '</div>';
+							echo $success_alert;
 						}
+						the_content();
 
-						 the_content();
-						 ?>
-						 <?php
-						 	if (!$isPressRelease && $isBoardMeeting && !isset($_GET['success'])):
-						 		if ($commentFormIsClosed):
-						 			echo "<p>The deadline for public comments for this meeting has passed.</p>";
-						 		else:
-						 ?>
-									<h3>Public Comments Form</h3>
-									<p>Public comments related to U.S. international media are now being accepted for review by the board. Comments intended for the <?php echo $postDate; ?> meeting of the board must be submitted by <b><?php echo $commentFormCloseStr; ?></b>.</p>
-									<p>Comments received after that date will be forwarded to the board for the following meeting.</p>
+						if (!$isPressRelease && $isBoardMeeting && !isset($_GET['success'])):
+					 		if ($commentFormIsClosed):
+					 			echo "<p>The deadline for public comments for this meeting has passed.</p>";
+					 		else:
+					?>
+								<h3>Public Comments Form</h3>
+								<p>Public comments related to U.S. international media are now being accepted for review by the board. Comments intended for the <?php echo $postDate; ?> meeting of the board must be submitted by <b><?php echo $commentFormCloseStr; ?></b>.</p>
+								<p>Comments received after that date will be forwarded to the board for the following meeting.</p>
 
-									<p>The public comments you provide to the Broadcasting Board of Governors are collected by the agency voluntarily and may be publicly disclosed on the Internet and/or via requests submitted to the BBG under the Freedom of Information Act.</p>
+								<p>The public comments you provide to the Broadcasting Board of Governors are collected by the agency voluntarily and may be publicly disclosed on the Internet and/or via requests submitted to the BBG under the Freedom of Information Act.</p>
 
-									<p>By providing public comments, you are consenting to their use and consideration by the Board and to their possible public dissemination. Personal contact information will not be made available to the public and will only be used by agency staff to engage with submitters regarding their own comments.</p>
-						<?php
-									$redirectLink = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+								<p>By providing public comments, you are consenting to their use and consideration by the Board and to their possible public dissemination. Personal contact information will not be made available to the public and will only be used by agency staff to engage with submitters regarding their own comments.</p>
+					<?php
+								$redirectLink = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-									if(strpos($redirectLink, "?" )) {
-										$redirectLink .= "&";
-									} else {
-										$redirectLink .= "?";
-									}
-									$redirectLink .= "success=true";
-									echo do_shortcode("[si-contact-form form='2' redirect='$redirectLink']");
-									echo '<script type="text/javascript" src="' . get_template_directory_uri() . '/js/meeting-comment-form.js"></script>';
-								endif;
+								if (strpos($redirectLink, "?")) {
+									$redirectLink .= "&";
+								} else {
+									$redirectLink .= "?";
+								}
+								$redirectLink .= "success = true";
+								echo do_shortcode("[si-contact-form form ='2' redirect = '$redirectLink']");
+								echo '<script type="text/javascript" src="' . get_template_directory_uri() . '/js/meeting-comment-form.js"></script>';
 							endif;
-
-							//echo "<pre>"; var_dump($_POST); echo "</pre>";
+						endif;
+					?>
+					</div><!-- END .bbg__article-content -->
+				</div><!-- END .page-post-main-content -->
+				
+				<!-- SIDEBAR -->
+				<div class="page-sidebar">
+					<div class="post-additional-information">
+						<?php
+							$event_info  = '<p class="sidebar-paragraph-head">';
+							$event_info .= 		'<a href="/news/events/">' . $eventPageHeader . ':</a>';
+							$event_info .= 		$meetingTime . ' ' . $postDate . '<br>' . $meetingLocation;
+							$event_info .= '</p>';
+							$event_info .= $eventBriteButtonStr;
+							$event_info .= '<p class="bbg-tagline bbg-tagline--main">';
+							$event_info .= 	$meetingContactTagline;
+							$event_info .= '</p>';
+							echo $event_info;
 						?>
+						<div class="share-social">
+							<h6>Share</h6>
+							<a href="<?php echo $fbUrl; ?>">
+								<span class="bbg__article-share__icon facebook"></span>
+							</a>
 
-					</div><!-- .entry-content -->
-
-					<div class="bbg__article-sidebar">
-						<h5>WHEN: <?php echo $meetingTime; ?><br/><?php echo $postDate; ?></h5>
-						<h5>WHERE: <?php echo $meetingLocation; ?></h5>
-
-						<?php echo $eventBriteButtonStr; ?>
-
-						<p class="bbg-tagline bbg-tagline--main"><?php echo $meetingContactTagline; ?></p>
-
+							<a href="<?php echo $twitterURL; ?>">
+								<span class="bbg__article-share__icon twitter"></span>
+							</a>
+						</div>
 						<!-- SPEAKERS -->
 						<?php
 							if ( have_rows('board_meeting_speakers') ) {
@@ -337,40 +292,33 @@ get_header(); ?>
 							}
 						?>
 
-						<!-- Related documents -->
+						<!-- RELATED DOCUMENTS -->
 						<?php
-							if ( have_rows('board_meeting_related_documents') ):
+							if ( have_rows('board_meeting_related_documents') ) {
 							 	echo '<h3 class="bbg__sidebar-label">Meeting documents</h3>';
 							 	echo '<ul class="bbg__profile__related-link__list">';
-							    while ( have_rows('board_meeting_related_documents') ) : the_row();
+							    while ( have_rows('board_meeting_related_documents') ) { 
+							    	the_row();
 							        echo '<li class="bbg__profile__related-link">';
 							        $dl = get_sub_field('board_meeting_related_document');
 							        echo "<a href='" . $dl['url'] . "'>" . $dl['title'] . "</a>";
 							        echo '</li>';
-							    endwhile;
+							    }
 							    echo '</ul>';
-							endif;
-						?>
+							}
 
-						<!-- Additional sidebar content -->
-						<?php
-							echo "<!-- Sidebar content -->";
+							echo "<!-- Additional Sidebar Content -->";
 							if ( $includeSidebar) {
 								echo $sidebar;
 							}
-
 							echo $sidebarDownloads;
 						?>
-					</div><!-- .bbg__article-sidebar -->
 
-				</div><!-- .usa-grid -->
-
-			</article><!-- #post-## -->
-
-		<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</div><!-- .post-additional-information -->
+				</div>
+		</article><!-- #post-## -->
+	<?php endwhile; // END POST LOOP ?>
+</main><!-- #main -->
 
 	<section class="usa-grid">
 		<?php get_sidebar(); ?>
