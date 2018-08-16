70938dcde5c8219d11d48ea53249c2525675df05
Kirk Radish <kirk@kirkradish.com>
2018-08-16 18:10:04 +0000

    Update Styles of Archive Template, Still Working with Grid

--- archive.php
+++ archive.php
@@ -7,90 +7,77 @@
  * @package bbgRedesign
  */
 
-get_header(); ?>
+get_header();
+require 'inc/bbg-functions-assemble.php';
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<div class="usa-grid-full">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php if ( have_posts() ) : ?>
-				<div class="usa-grid">
-					<header class="page-header">
-						<?php
-							the_archive_title( '<h5 class="bbg__label--mobile large">', '</h5>' );
-						?>
-					</header><!-- .page-header -->
-				</div>
+		<?php
+			if (have_posts()) :
+				// $page_header  = '<div class="outer-container">';
+				// $page_header .= 	'<div class="grid-container">';
+				// $page_header .= 		'<h2>' . get_the_archive_title() . '</h2>';
+				// $page_header .= 	'</div>';
+				// $page_header .= '</div>';
+				// echo $page_header;
 
-				<?php /* Start the Loop */
-					$counter = 0;
-					while ( have_posts() ) : the_post();
-						$counter++;
-						/*if( $counter == 1 ) {
-							echo '<div class="usa-grid-full">';
-							echo '<div class="bbg-grid--1-1-1-2 secondary-stories">';
-						} elseif( $counter==3 ){
+				$counter = 0;
+				while (have_posts()) : the_post();
+					$counter++;
+
+					// ONLY SHOW FEATURED IF IT'S NOT PAGINATED
+					if ((!is_paged() && $counter == 1 || is_category('BBG360'))) {
+						$featured_media_result = get_feature_media_data();
+						if ($featured_media_result != "") {
+							echo $featured_media_result;
+						}
+
+						echo '<div class="outer-container">';
+						echo 	'<div class="grid-container">';
+						echo 		'<h3>' . get_the_title() . '</h3>';
+						echo 		'<span class="lead-in">' . get_the_excerpt() . '</span>';
+						echo 	'</div>';
+						echo '</div>';
+					} else {
+						if ((!is_paged() && $counter == 2) || (is_paged() && $counter == 1)) {
+							echo '</div><!-- huh -->';
+							echo '<div class="usa-grid">';
+							echo 	'<div class="bbg-grid--1-1-1-2 secondary-stories">';
+						}
+						elseif ((!is_paged() && $counter == 4) || (is_paged() && $counter == 3)) {
 							echo '</div><!-- left column -->';
 							echo '<div class="bbg-grid--1-1-1-2 tertiary-stories">';
-							echo '<header class="page-header">';
-							echo '<h6 class="page-title bbg__label small">More news</h6>';
-							echo '</header>';
+							$moreLabel = "More News";
+							if (is_category('Board Meetings')) {
+								$moreLabel = "More Board Meetings";
+							}
+							echo 	'<h5>' . $moreLabel . '</h5>';
 
 							//These values are used for every excerpt >=4
-							$includeImage = FALSE;
-							$includeMeta = FALSE;
-							$includeExcerpt=FALSE;
+							$includeImage = false;
+							$includeMeta = false;
+							$includeExcerpt = false;
 						}
+						get_template_part('template-parts/content-excerpt-list', get_post_format());
+					}
 
-						get_template_part( 'template-parts/content-excerpt-list', get_post_format() );
-						*/
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+				endwhile;
+				the_posts_navigation();
+			else :
+				get_template_part('template-parts/content', 'none');
+			endif;
+		?>
 
-						//Add a check here to only show featured if it's not paginated.
-						if ( (!is_paged() && $counter == 1 || is_category('BBG360')) ){
-							get_template_part( 'template-parts/content-excerpt-featured', get_post_format() );
-						} else {
-							if( (!is_paged() && $counter == 2) || (is_paged() && $counter == 1) ){
-								echo '</div>';
-								echo '<div class="usa-grid">';
-									echo '<div class="bbg-grid--1-1-1-2 secondary-stories">';
-							} elseif( (!is_paged() && $counter == 4) || (is_paged() && $counter == 3)){
-								echo '</div><!-- left column -->';
-								echo '<div class="bbg-grid--1-1-1-2 tertiary-stories">';
-									echo '<header class="page-header">';
-									$moreLabel = "More News";
-
-									if (is_category('Board Meetings')) {
-										$moreLabel = "More Board Meetings";
-									}
-										echo '<h6 class="page-title bbg__label small">' . $moreLabel . '</h6>';
-									echo '</header>';
-
-								//These values are used for every excerpt >=4
-								$includeImage = FALSE;
-								$includeMeta = FALSE;
-								$includeExcerpt = FALSE;
-							}
-							get_template_part( 'template-parts/content-excerpt-list', get_post_format() );
-						}
-
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-					?>
-
-					<?php endwhile; ?>
-
-					<?php the_posts_navigation(); ?>
-				</div><!-- .usa-grid -->
-
-				<?php else : ?>
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
-				<?php endif; ?>
-			</div><!-- .usa-grid -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div><!-- .usa-grid -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
