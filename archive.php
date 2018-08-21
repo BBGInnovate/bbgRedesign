90de2f95d0c4b411464ba142ed0f16be8c4bcd18
Kirk Radish <kirk@kirkradish.com>
2018-08-21 16:44:21 +0000

    Update Grid for archive Template

--- archive.php
+++ archive.php
@@ -26,7 +26,11 @@ require 'inc/bbg-functions-assemble.php';
 				$counter = 0;
 				while (have_posts()) : the_post();
 					$counter++;
-
+					if ($counter < 4) {
+						$in_sidebar = false;
+					} else {
+						$in_sidebar = true;
+					}
 					// ONLY SHOW FEATURED IF IT'S NOT PAGINATED
 					if ((!is_paged() && $counter == 1 || is_category('BBG360'))) {
 						$featured_media_result = get_feature_media_data();
@@ -40,15 +44,22 @@ require 'inc/bbg-functions-assemble.php';
 						echo 		'<span class="lead-in">' . get_the_excerpt() . '</span>';
 						echo 	'</div>';
 						echo '</div>';
-					} else {
+					} 
+
+					if ($counter == 2) {
+						echo '<div class="outer-container">';
+						echo 	'<div class="custom-grid-container">';
+						echo 		'<div class="inner-container">';
+					}
+
+					if ((!is_paged() && $counter > 1)) {
 						if ((!is_paged() && $counter == 2) || (is_paged() && $counter == 1)) {
-							echo '</div><!-- huh -->';
-							echo '<div class="usa-grid">';
-							echo 	'<div class="bbg-grid--1-1-1-2 secondary-stories">';
+							echo '</div>';
+							echo 	'<div class="main-content-container">';
 						}
 						elseif ((!is_paged() && $counter == 4) || (is_paged() && $counter == 3)) {
-							echo '</div><!-- left column -->';
-							echo '<div class="bbg-grid--1-1-1-2 tertiary-stories">';
+							echo '</div>';
+							echo '<div class="side-content-container">';
 							$moreLabel = "More News";
 							if (is_category('Board Meetings')) {
 								$moreLabel = "More Board Meetings";
@@ -60,7 +71,17 @@ require 'inc/bbg-functions-assemble.php';
 							$includeMeta = false;
 							$includeExcerpt = false;
 						}
-						get_template_part('template-parts/content-excerpt-list', get_post_format());
+						// get_template_part('template-parts/content-excerpt-list', get_post_format());
+						$article_markup  = '<article id="'. get_the_ID() . '" style="margin-bottom: 1.5rem">';
+						if ($in_sidebar == false) {
+							$article_markup .= '<h4><a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a></h4>';
+							$article_markup .= '<p class="aside date-meta">' . get_the_date() . '</p>';
+							$article_markup .= '<p>' . get_the_excerpt() . '</p>';
+						} else {
+							$article_markup .= '<h6><a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a></h6>';
+						}
+						$article_markup .= '</article>';
+						echo $article_markup;
 					}
 
 					/*
@@ -69,7 +90,11 @@ require 'inc/bbg-functions-assemble.php';
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
 				endwhile;
+
 				the_posts_navigation();
+				echo 		'</div>';
+				echo 	'</div>';
+				echo '</div>';
 			else :
 				get_template_part('template-parts/content', 'none');
 			endif;
