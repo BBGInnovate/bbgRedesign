5552c697e1e72194a85a32804b23968398860e67
Kirk Radish <kirk@kirkradish.com>
2018-09-05 21:10:37 +0000

    New USAGM Splash Page

--- js/usagm-splash.js
+++ js/usagm-splash.js
@@ -2,59 +2,8 @@
 $('document').ready(function() {
 
 if ($('#usagm-splash-wrapper').length != 0) {
-	var logoSrc = $('.logo-board img.logo').attr('src');
-	var imgFromPath = logoSrc.split("/");
-	var imgFile = imgFromPath[imgFromPath.length - 1];
-	var bbgLogoFileName = imgFromPath[imgFromPath.length - 1];
-	var usagmLogoFileName = "usagm-splash-logo.png";
-	var usagmLogoPath = logoSrc.replace(imgFromPath[imgFromPath.length - 1], usagmLogoFileName);
-	var bbgLogoFilepath = logoSrc.replace(imgFromPath[imgFromPath.length - 1], bbgLogoFileName);
-
-	function getImgFiles() {
-		var logoSrc = $('.logo-board img.logo').attr('src');
-		var imgFromPath = logoSrc.split("/");
-		var imgFileName = imgFromPath[imgFromPath.length - 1];
-		return imgFileName;
-	}
-
-	function slideScroll() {
-		var bbgCopyBottom = $('#bbg-copy').offset().top + $('#bbg-copy').outerHeight();
-		var usagmCopyBottom = $('#usagm-copy').offset().top + $('#usagm-copy').outerHeight();
-		var shifter;
-		if ($(window).scrollTop() < $('#bbg-copy').offset().top) {
-			shifter = bbgCopyBottom;
-		}
-		else {
-			shifter = usagmCopyBottom;
-		}
-		$('html, body').animate({
-			scrollTop: shifter
-		}, 700);
-	}
-
-	// MOVE DOWN ARROW ON HOVER
-	var logoPageScroller = $('.splash-down');
-	logoPageScroller.hover(function() {
-		$(this).animate({'padding-top': '5px'});
-	}, function() {
-		$(this).animate({'padding-top': '0'});
-	});
-	logoPageScroller.on('click', function() {
-		slideScroll();
-	});
-
-	// PARALLAX BBG LOGO
-	function scrollLogoTopPos(elem) {
-		var thisFarDownPage = $(window).scrollTop();
-		elem.css('top', thisFarDownPage);
-	}
-	scrollLogoTopPos($('#bbg-logo-container'));
-	$(window).on('scroll', function() {
-		scrollLogoTopPos($('#bbg-logo-container'));
-	});
-
 	// LIGHTBOX
-	var lb = $('.lightbox');
+	var lb = $('.lightbox-shell');
 	var lbLinks = $('.lightbox-link');
 	var lbLinksRefs = lbLinks.attr('href');
 	var lbBg = $('<div class="lb-bg"></div>');
@@ -93,13 +42,13 @@ if ($('#usagm-splash-wrapper').length != 0) {
 		lbVideoBox.append(closeBu);
 		$('body').prepend(lbBg);
 		if (curId == "what-we-do") {
-			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/_j94Vc-8zyg?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/_j94Vc-8zyg?rel=0?playsinline=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
 		}
 		else if (curId == "who-we-are") {
-			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/z4XWcruGhNk?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/z4XWcruGhNk?rel=0?playsinline=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
 		}
 		else if (curId == "ceo-message") {
-			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/eTNV0cnb6No?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+			lbVideoBox.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/eTNV0cnb6No?rel=0?playsinline=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
 		}
 	});
 
--- usagm-intro.php
+++ usagm-intro.php
@@ -4,69 +4,49 @@
   template name: USAGM-Intro
  */
 get_header();
+// echo phpinfo();
 ?>
 <div id="usagm-splash-wrapper">
 
-<div class="logo-board locked">
-	<div class="logo-container" id="bbg-logo-container">
-		<img class="logo" id="bbg-logo" src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/bbg-splash-logo.png">
+<div class="logo-board">
+	<div class="logo-container">
+		<img class="logo" id="bbg-logo" src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/usagm-splash-logo.png">
 	</div>
-</div>
-
-<div id="page-mover">
-	<div class="logo-copy" id="bbg-copy">
-		<div class="logo-fader-up"></div>
-		<div class="center-copy-block">
-			<p>The Broadcasting Board of Governors is&nbsp;now&nbsp;the<br>United States Agency for Global Media.</p>
-			<p class="splash-down"><i class="fas fa-angle-down"></i></p>
-		</div>
-	</div>
-
-	<div class="logo-board">
-		<div class="logo-container">
-			<img class="logo" id="bbg-logo" src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/usagm-splash-logo.png">
-		</div>
-	</div>
-
-	<div class="logo-copy" id="usagm-copy">
-		<div class="center-copy-block">
-			<p>New name, same mission.</p>
-			<p class="splash-down"><i class="fas fa-angle-down"></i></p>
-		</div>
+	<div class="logo-copy">
+		<p>The Broadcasting Board of Governors is&nbsp;now&nbsp;the<br>United States Agency for Global Media.</p>
+		<p>New name, same mission.</p>
 	</div>
 </div>
 
 <div class="messaging-section">
 	<div class="outer-container">
-		<div class="grid-container">
-			<div class="inner-container message">
-				<div class="grid-container lightbox">
-					<div class="lightbox-link" id="what-we-do">
-						<div class="message-image">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/what-we-do.jpg" alt="">
-							<h4>What We Do</h4>
-						</div>
+		<div class="message">
+			<div class="lightbox-shell">
+				<div class="lightbox-link" id="what-we-do">
+					<div class="message-image">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/what-we-do.jpg" alt="">
+						<h4>What We Do</h4>
 					</div>
 				</div>
 			</div>
-			<div class="inner-container message">
-				<div class="grid-container lightbox">
-					<div class="lightbox-link" id="who-we-are">
-						<div class="message-image">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/who-we-are.jpg" alt="">
-							<h4>Who We Are</h4>
-						</div>
+		</div>
+		<div class="message">
+			<div class="lightbox-shell">
+				<div class="lightbox-link" id="who-we-are">
+					<div class="message-image">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/who-we-are.jpg" alt="">
+						<h4>Who We Are</h4>
 					</div>
 				</div>
 			</div>
-			<div class="inner-container message">
-				<div class="grid-container lightbox">
-					<div class="lightbox-link" id="ceo-message">
-						<div class="message-image">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/ceo-message.jpg" alt="">
-						</div>
-						<h4>Message from the CEO</h4>
+		</div>
+		<div class="message">
+			<div class="lightbox-shell">
+				<div class="lightbox-link" id="ceo-message">
+					<div class="message-image">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/usagm-splash/ceo-message.jpg" alt="">
 					</div>
+					<h4>Message from the CEO</h4>
 				</div>
 			</div>
 		</div>
@@ -85,12 +65,8 @@ get_header();
 	</div>
 </div>
 
-<div class="messaging-section" id="site-link">
-	<div class="outer-container">
-		<div class="grid-container">
-			<p style="font-size: 3.5rem; text-transform: uppercase; font-weight: 300; letter-spacing: 2px;"><a class="site-brand" href="<?php echo get_home_url(); ?>/home">Continue to usagm.gov</a></p>
-		</div>
-	</div>
+<div id="site-link">
+	<button type="button"><a class="site-brand" href="https://www.usagm.gov/">Continue to USAGM.gov</a></button>
 </div>
 
 </div> <!-- END #usagm-splash-wrapper -->
