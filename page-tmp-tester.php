50508b3e527404ab043cf888731f1a3ab9e94792
Kirk Radish <kirk@kirkradish.com>
2018-09-06 21:16:02 +0000

    Delete file for testing

---
+++ page-tmp-tester.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying 2 column pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package bbgRedesign
+   template name: tmpTMP
+ */
+
+?>
