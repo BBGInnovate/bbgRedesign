ce8ba8db22244f4576e75786b26fa74d9f1f17c4
Kirk Radish <kirk@kirkradish.com>
2018-09-28 20:17:41 +0000

    Fix Archive Grid

--- archive.php
+++ archive.php
@@ -13,6 +13,7 @@ require 'inc/bbg-functions-assemble.php';
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+
 		<?php
 			if (have_posts()) :
 				$counter = 0;
@@ -38,18 +39,18 @@ require 'inc/bbg-functions-assemble.php';
 						echo '</div>';
 					} 
 
-					if ((!is_paged() && $counter == 2) || is_paged() && $counter == 1) {
+					if ($counter == 2) {
 						echo '<div class="outer-container">';
 						echo 	'<div class="custom-grid-container">';
 						echo 		'<div class="inner-container">';
 					}
 
-					if ((!is_paged() && $counter > 1) || (is_paged() && $counter > 0)) {
+					if ((!is_paged() && $counter > 1)) {
 						if ((!is_paged() && $counter == 2) || (is_paged() && $counter == 1)) {
 							echo '</div>';
-							echo 	'<div class="main-content-container">';
+							echo '<div class="main-content-container">';
 						}
-						elseif ((!is_paged() && $counter == 4) || (is_paged() && $counter == 4)) {
+						elseif ((!is_paged() && $counter == 4) || (is_paged() && $counter == 3)) {
 							echo '</div>';
 							echo '<div class="side-content-container">';
 							$moreLabel = "More News";
@@ -73,7 +74,7 @@ require 'inc/bbg-functions-assemble.php';
 				endwhile;
 
 				echo 		'</div>';
-				the_posts_navigation();
+				echo get_the_posts_navigation();
 				echo 	'</div>';
 				echo '</div>';
 			else :
