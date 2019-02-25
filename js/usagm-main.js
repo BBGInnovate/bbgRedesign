a79d9f62b74cb9e1506eafdd0666d8df7cba091c
Kirk Radish <kirk@kirkradish.com>
2019-02-25 15:15:49 +0000

    Remove HD Proportion Sizing for Banner Image and Revert Back to Old Rules for Height of Banner

--- js/usagm-main.js
+++ js/usagm-main.js
@@ -8,42 +8,6 @@ if (!($('#main').prev().hasClass('page-featured-media') || $('#main').prev().has
 	}
 }
 
-// function newHomeMainSpace() {
-// 	if ((top.location.pathname === '/new-homepage-test/')) {
-// 		if ($(window).width() > 685) {
-// 			// $('#main').css('padding-top', '6rem');
-// 		}
-// 		else {
-// 			$('#main').css('padding-top', '0');
-// 		}
-// 	}
-// }
-// newHomeMainSpace();
-
-// $(window).on('resize', function() {
-// 	newHomeMainSpace();
-// })
-
-// KEEPS FEATURED MEDIA SCALED AT HD PROPORTIONS
-function featuredMediaHD() {
-	var hd_scale = 0.5625;
-	var containerW = $('.page-featured-media').width();
-	if ($('.page-featured-media').children('bbg-banner')) {
-		if (($('iframe').length > 0) && ($('iframe').attr('src').indexOf('youtube') != 0)) {
-			$('.bbg-banner').width('100%');
-			containerW = $('iframe.bbg-banner').width();
-		}
-	} else {
-		$('.bbg-banner').width(containerW);
-	}	
-	var dynHeight = containerW * hd_scale;
-	$('.bbg-banner').height(dynHeight);
-}
-featuredMediaHD();
-$(window).on('resize', function() {
-	featuredMediaHD();
-});
-
 // KEEPS BACKGROUND IMAGES A CONSISTENT SIZE
 function sizeBGimages() {
 	var img_scale = 1.77778;
@@ -122,39 +86,6 @@ if ($('.sidebar-section').length > 0) {
 }
 
 
-// TEST NEW HOME
-// var windowWidth = 0;
-// function resizeEntityBoxes() {
-// 	windowWidth = $(window).width();
-// 	var entityBoxW = $('.network-entity-chunk').width();
-// 	if (windowWidth < 585) {
-// 		$('.network-entity-chunk').height('85px');
-// 		$('.inner-entity').css({
-// 			'height': '85px',
-// 			'width': entityBoxW
-// 		});
-// 		$('.entity-title.entity-voa').text('VOA');
-// 		$('.entity-title.entity-rferl').text('RFE/RL');
-// 		$('.entity-title.entity-ocb').text('OCB');
-// 		$('.entity-title.entity-rfa').text('RFA');
-// 		$('.entity-title.entity-mbn').text('MBN');
-// 	}
-// 	else {
-// 		$('.network-entity-chunk').height(entityBoxW);
-// 		$('.inner-entity').css({
-// 			'height': entityBoxW,
-// 			'width': entityBoxW
-// 		});
-// 		$('.entity-title.entity-voa').text('Voice of America');
-// 		$('.entity-title.entity-rferl').text('Radio Free Europe/Radio Liberty');
-// 		$('.entity-title.entity-ocb').text('Office of Cuba Broadcasting');
-// 		$('.entity-title.entity-rfa').text('Radio Free Asia');
-// 		$('.entity-title.entity-mbn').text('Middle East Broadcasting Network');
-// 	}
-// }
-// resizeEntityBoxes();
-
-
 // FIVE GRID BOX MODULE: Hover
 if ($('.grid-box-chunk').length > 0) {
 	$('.grid-box-text').hide();
@@ -178,34 +109,7 @@ if ($('.grid-box-chunk').length > 0) {
 		})
 	});
 }
-// FIVE GRID BOX MODULE: Size
-// function setFiveGridBox() {
-// 	windowWidth = $(window).width();
-// 	var entityBoxW = $('.grid-box-chunk').width();
-// 	if (windowWidth < 585) {
-// 		$('.grid-box-chunk').height('85px');
-// 	}
-// 	else {
-// 		$('.grid-box-chunk').height(entityBoxW);
-// 	}
-// }
-// setFiveGridBox();
 
-// function resizePostImage() {
-// 	// SCALE EACH POST IMATE HEIGHT TO 35% OF WIDTH
-// 	var scalePcx = 0.35;
-// 	var dynamicHeight = 0;
-// 	var postImageWidth = 0;
-// 	var postImage = $('#new-home-test .post-image');
-
-// 	$.each(postImage, function() {
-// 		postImageWidth = $(this).parent().width();
-// 		dynamicHeight = postImageWidth * scalePcx;
-// 		dynamicHeight = postImageWidth - dynamicHeight;
-// 		$(this).height(dynamicHeight);
-// 	});
-// }
-// resizePostImage();
 
 function scaleRibbonBanner() {
 	setTimeout(function() {
@@ -235,9 +139,6 @@ function scaleCornerHero() {
 scaleCornerHero();
 
 $(window).on('resize', function() {
-	// resizeEntityBoxes();
-	// setFiveGridBox();
-	// resizePostImage();
 	scaleRibbonBanner();
 	scaleCornerHero();
 });
