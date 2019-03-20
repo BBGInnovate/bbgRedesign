838d184cf177ac173dd8005abbc43d1d714b2bb1
Kirk Radish <kirk@kirkradish.com>
2019-03-20 20:41:27 +0000

    Add Lazy Load Plugin on Index Page

--- inc/article-structures-markup.php
+++ inc/article-structures-markup.php
@@ -1,7 +1,7 @@
 <?php
 // STYLES FOR THESE ARTICLE TEASERS ARE IN _scss/bbg/components/_article-structure-style.scss
 
-function build_main_head_article() {
+function build_main_head_article($article_data) {
 	$article_structure  = '<article class="main-head-article article-teaser">';
 	if (!empty(get_the_permalink($article_data))) {
 		$article_structure .= '<div class="feature-article-image">';
@@ -13,6 +13,8 @@ function build_main_head_article() {
 	$article_structure .= 		'<p class="date-meta">' . get_the_date('F j, Y', $article_data) . '</p>';
 	if (!empty($article_data->post_excerpt)) {
 		$article_structure .= 		'<p class="excerpt">' . $article_data->post_excerpt . ' <a class="read-more" href="' . get_the_permalink($article_data) . '">Read More</a></p>';
+	} else {
+		$article_structure .= 	'<p class="excerpt">' . wp_trim_words($article_data->post_content, 60) . ' <a class="read-more" href="' . get_the_permalink($article_data) . '">Read More</a></p>';
 	}
 	$article_structure .= 	'</div>';
 	$article_structure .= '</article>';
@@ -91,10 +93,10 @@ function build_horizontal_small_image_article($article_data) {
 	$article_structure .= 		'<div class="inner-container">';
 	if (get_the_post_thumbnail($article_data)) {
 		if (!empty(get_the_permalink($article_data))) {
-			$article_structure .= 			'<div class="article-image"><a href="' . get_the_permalink($article_data) . '">' . get_the_post_thumbnail($article_data, 'large') . '</a></div>';
+			$article_structure .= 			'<div class="grid-container article-image"><a href="' . get_the_permalink($article_data) . '">' . get_the_post_thumbnail($article_data, 'large') . '</a></div>';
 		}
 	}
-	$article_structure .= 			'<div class="article-desc article-info">';
+	$article_structure .= 			'<div class="grid-container article-desc article-info">';
 	$article_structure .= 				'<h4><a href="' . get_the_permalink($article_data) . '">' . get_the_title($article_data) . '</a></h4>';
 	$article_structure .= 				'<p class="date-meta">' . get_the_date('F j, Y', $article_data) . '</p>';
 	if (!empty($article_data->post_excerpt)) {
@@ -145,7 +147,7 @@ function build_horizontal_image_title_only_article($article_data) {
 	$article_structure .= 		'<div class="inner-container">';
 	if (!empty(get_the_post_thumbnail($article_data))) {
 		if (!empty(get_the_permalink($article_data))) {
-			$article_structure .= 			'<div class="article-image"><a href="' . get_the_permalink($article_data) . '">' . get_the_post_thumbnail($article_data, 'large') . '</a></div>';
+			$article_structure .= 	'<div class="article-image"><a href="' . get_the_permalink($article_data) . '">' . get_the_post_thumbnail($article_data, 'large') . '</a></div>';
 		}
 	}
 	$article_structure .= 			'<div class="article-desc article-info">';
--- index.php
+++ index.php
@@ -12,13 +12,17 @@
  * @package bbgRedesign
  */
 
-// $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-// $posts_query_params = array(
-// 	'post_type' => 'post',
-// 	'paged' => $paged
-// );
-// $news_posts_query = new WP_Query($posts_query_params);
-// $all_news_posts = $news_posts_query->posts;
+$feature_post_arg = array(
+	'post_type' => 'post',
+	'posts_per_page' => 1
+);
+$feature_post_query = new WP_Query($feature_post_arg);   
+$feature_post = $feature_post_query->posts;
+$do_not_duplicate[] = $feature_post[0]->ID;
+wp_reset_query();   
+
+// LOAD USED IDs ARRAY INTO AJAX LOAD MORE PLUGIN
+$post__not_in = ($do_not_duplicate) ? implode(',', $do_not_duplicate) : '';     
 
 get_header();
 ?>
@@ -29,91 +33,17 @@ get_header();
 		<div class="grid-container">
 			<h2><?php echo single_post_title(); ?></h2>
 		</div>
-	<?php
-	// if ((!is_paged())) {
-	// 	echo '<div class="grid-container">';
-	// 	echo 	build_main_head_article($all_news_posts[0]);
-	// 	echo '</div>';
-	// 	array_shift($all_news_posts);
-	// }
-
-	// echo '<div class="archive-column">';
-	// foreach($all_news_posts as $cur_article) {
-	// 	echo build_horizontal_image_title_only_article($cur_article);
-	// }
-	// echo '</div>';
-
-	// echo '<div class="outer-container">';
-	// echo 	'<div class="grid-container">';
-	// the_posts_navigation();
-	// echo 	'</div>';
-	// echo '</div>';
-	?>
-	</div>
-	<?php
-		// PAGINATION DOES NOT WORK THIS WAY
-		if (have_posts()) :
-			$counter = 0;
-			while (have_posts()) : the_post();
-				$counter++;
-				if ($counter < 4) {
-					$in_sidebar = false;
-				} else {
-					$in_sidebar = true;
-				}
-				// ONLY SHOW FEATURED IF IT'S NOT PAGINATED
-				if ((!is_paged() && $counter == 1)) {
-					$thumbnail_image = get_posts(array('p' => get_post_thumbnail_id($id), 'post_type' => 'attachment'));
-					$banner_position = get_field('adjust_the_banner_image', '', true);
-					$src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), array(700, 450), false, '');
-					echo '<div class="outer-container">';
-					echo 	'<div class="grid-container">';
-					echo 		'<article id="post-' . get_the_ID() . '">';
-					echo 			'<div class="bbg__article-header__banner" ';
-					echo 				'style="background-image: url(' . $src[0] . '); background-position: ' . $banner_position . '">';
-					echo 			'</div>';
-					echo 		'<h4><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></h4>';
-					echo 		'<p class="aside date-meta">' . get_the_date() . '</p>';
-					echo 		'<p>' . get_the_excerpt() . '</p>';
-					echo '</article>';
-					echo 	'</div>';
-					echo '</div>';
-				}
-				if ($counter == 2) {
-					echo '<div class="outer-container">';
-					echo 	'<div class="custom-grid-container">';
-					echo 		'<div class="inner-container">';
-				}
-				if ((!is_paged() && $counter > 1)) {
-					if ((!is_paged() && $counter == 2) || (is_paged() && $counter == 1)) {
-						echo '</div>';
-						echo 	'<div class="main-content-container">';
-					} elseif( (!is_paged() && $counter == 4) || (is_paged() && $counter == 3)){
-						echo '</div>';
-						echo '<div class="side-content-container">';
-						echo 	'<h5>More news</h5>';
-					}
-					$article_markup  = '<article id="'. get_the_ID() . '" style="margin-bottom: 1.5rem">';
-					if ($in_sidebar == false) {
-						$article_markup .= '<h4><a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a></h4>';
-						$article_markup .= '<p class="aside date-meta">' . get_the_date() . '</p>';
-						$article_markup .= '<p>' . get_the_excerpt() . '</p>';
-					} else {
-						$article_markup .= '<h6><a href="' . esc_url(get_permalink()) . '">' . get_the_title() . '</a></h6>';
-					}
-					$article_markup .= '</article>';
-					echo $article_markup;
-				}
-			endwhile;
-			the_posts_navigation();
-			echo 		'</div>';
-			echo 	'</div>';
+		<?php
+			echo '<div class="grid-container">';
+			echo 	build_main_head_article($feature_post[0]);
 			echo '</div>';
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-	?>
+
+			echo '<div class="grid-container">';
+			// AJAX LOAD MORE PLUGIN
+			echo 	do_shortcode('[ajax_load_more post__not_in="'. $post__not_in .'"]');
+			echo '</div>';
+		?>
+	</div>
 </main>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
