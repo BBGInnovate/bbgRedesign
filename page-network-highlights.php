96887f90c62ff51adbb6c6f1c89b7e86ee4256ba
Kirk Radish <kirk@kirkradish.com>
2019-04-19 19:40:26 +0000

    Fix HTML Outliners on Press Release Page

--- page-network-highlights.php
+++ page-network-highlights.php
@@ -120,10 +120,10 @@ get_header();
 
 
 						if ($counter == 1) {
-							$s .= 	'<h4><a href="' . $url . '">' . $title . '</a></h4>';
+							$s .= 	'<h4 class="article-title"><a href="' . $url . '">' . $title . '</a></h4>';
 							$s .= 	'<p>' . $pr['excerpt'] . '</p>';
 						} else {
-							$s .= 	'<h3 class="sidebar-article-title"><a href="' . $url . '">' . $title . '</a></h3>';
+							$s .= 	'<h4 class="sidebar-article-title"><a href="' . $url . '">' . $title . '</a></h4>';
 						}
 						if ($counter == 1 || $counter == 5) {
 							if ($counter == 5) {
@@ -132,7 +132,7 @@ get_header();
 			  					if ($entityString == 'bbg') {
 			  						$entityString = "usagm";
 			  					}
-								$s .= 	'<article><a href="' . get_category_link($id) . '">Read more ' . strtoupper($entityString) . ' news »</a></article>';
+								$s .= 	'<div><a href="' . get_category_link($id) . '">Read more ' . strtoupper($entityString) . ' news »</a></div>';
 							}
 							$s .= '</div>';
 						}
