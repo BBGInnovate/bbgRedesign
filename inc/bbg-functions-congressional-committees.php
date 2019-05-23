8b1d650c8fbb71b0e1ff978c11286498b84b3f30
Kirk Radish <kirk@kirkradish.com>
2019-05-23 20:12:58 +0000

    Replace Sunlight Foundation Congress API with the New ProPublica API

--- inc/bbg-functions-congressional-committees.php
+++ inc/bbg-functions-congressional-committees.php
@@ -1,84 +1,95 @@
 <?php 
-	//Grab the list of congressional committee members from the Sunlight Foundation's API
-	function getCongressionalCommittee($committeeID, $committeeTitle) {
-		$committeeFilepath = get_template_directory() . "/external-feed-cache/committeecache$committeeID.json";
-		if ( fileExpired($committeeFilepath, 1440) ) { 	//1440 min = 1 day
-			//use http://tryit.sunlightfoundation.com/congress to try out the api
-			$url="https://congress.api.sunlightfoundation.com/committees?apikey=" . SUNLIGHT_API_KEY . "&committee_id=$committeeID&fields=members";
-			$result=fetchUrl($url);
-			file_put_contents($committeeFilepath, $result);
+	// ProPublica Congress API
+	// https://projects.propublica.org/api-docs/congress-api/
+	function get_congressional_committee($congress, $chamber, $committee_id, $subcommittee_id, $title) {
+		$committee_title = $title;
+		if (!empty($subcommittee_id)) {
+			$api_url = 'https://api.propublica.org/congress/v1/' . $congress .'/'. $chamber . '/committees/' . $committee_id .'/subcommittees/'. $subcommittee_id .'.json';
 		} else {
-			$result=file_get_contents($committeeFilepath);
+			$api_url = 'https://api.propublica.org/congress/v1/' . $congress .'/'. $chamber . '/committees/' . $committee_id .'.json';
 		}
-		$c2 = json_decode($result, true);
-		$members=$c2["results"][0]["members"];
 
-		$minority=array();
-		$majority=array();
-		foreach($members as $m) {
-			if ($m['side']=='majority') {
-				$majority[]=$m;
+		// REQUEST DATA FROM API, RETURN STRING INSTEAD OF PRINTING ARRAY DIRECTLY TO SCREEN
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_HTTPHEADER => array('X-API-Key: ' . PROPUBLICA_CONGRESS_API_KEY),
+			CURLOPT_URL => $api_url
+		]);
+		$response = curl_exec($curl);
+		curl_close($curl);
+
+		// CONVERT JSON STRING TO ARRAY
+		$response_str_to_arr = json_decode($response, true);
+		$members = $response_str_to_arr['results'][0]['current_members'];
+
+		$majority_side = [];
+		$minority_side = [];
+		for ($i = 0; $i < count($members); $i++) {			
+			if ($members[$i]['side'] == 'majority') {
+				$majority_side[] = $members[$i];
 			} else {
-				$minority[]=$m;
+				$minority_side[] = $members[$i];
 			}
 		}
 
-	$s='<div class="usa-accordion bbg__committee-list">';
-	$s.='<ul class="usa-unstyled-list">';
-	$s.='<li>';
-	$s.='<button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-'.$committeeID.'">';
-	$s.=$committeeTitle;
-	$s.='</button>';
-	$s.='<div id="collapsible-'.$committeeID.'" aria-hidden="true" class="usa-accordion-content">';
-
-
-
-
-		$s.="<section class='usa-grid-full'>";
-		$s.= "<div class='bbg-grid--1-1-1-2'>";
-		$s.= "<strong>MAJORITY</strong> (".$majority[0]['legislator']['party'].")";
-		$s.= "<ul class='usa-unstyled-list'>";
-		foreach ($majority as $m) {
-			$firstName=$m['legislator']['first_name'];
-			$lastName=$m['legislator']['last_name'];
-			$state=$m['legislator']['state'];
-			$title='';
-			if (isset($m['title'])) {
-				$title=' <em>— '.$m['title'].'</em>';
+		$committee_markup  = '<div class="usa-accordion bbg__committee-list">';
+		$committee_markup .= 	'<ul class="usa-unstyled-list">';
+		$committee_markup .= 		'<li>';
+		$committee_markup .= 			'<h4 class="article-title">';
+		$committee_markup .= 				'<button class="usa-button-unstyled" aria-expanded="false" aria-controls="collapsible-' . $committee_id . '">';
+		$committee_markup .= 					$committee_title;
+		$committee_markup .= 				'</button>';
+		$committee_markup .= 			'</h4>';
+		$committee_markup .= 			'<div id="collapsible-' . $committee_id . '" aria-hidden="true" class="usa-accordion-content">';
+		$committee_markup .= 				'<div class="grid-container">';
+		$committee_markup .= 					'<div class="grid-half">';
+		$committee_markup .= 						'<span class="paragraph-header">MAJORITY (' . strtoupper($majority_side[0]['party']) . ')</span>';
+		$committee_markup .= 						'<ul class="unstyled-list">';
+		for ($i = 0; $i < count($majority_side); $i++) {
+			$committee_markup .= 						'<li>';
+			$committee_markup .= 							$majority_side[$i]['name'] . ', ' . $majority_side[$i]['state'];
+			if ($majority_side[$i]['rank_in_party'] == 1) {
+				$committee_markup .= 							'&mdash;' . $majority_side[$i]['note'];
 			}
-			$s.= "<li>$firstName $lastName, $state $title</li>";
+			$committee_markup .= 						'</li>';
 		}
-		$s.= "</ul>";
-		$s.= "</div><!-- .bbg-grid -->";
-		$s.= "<div class='bbg-grid--1-1-1-2'>";
-		$s.=  "<strong>MINORITY</strong> (".$minority[0]['legislator']['party'].")";
-		$s.= "<ul class='usa-unstyled-list'>";
-		foreach ($minority as $m) {
-			$firstName=$m['legislator']['first_name'];
-			$lastName=$m['legislator']['last_name'];
-			$state=$m['legislator']['state'];
-			$title='';
-			if (isset($m['title'])) {
-				$title=' <em>— '.$m['title'].'</em>';
+		$committee_markup .= 						'</ul>';
+		$committee_markup .= 					'</div>';
+		$committee_markup .= 					'<div class="grid-half">';
+		$committee_markup .= 						'<span class="paragraph-header">MINORITY (' . strtoupper($minority_side[0]['party']) . ')</span>';
+		$committee_markup .= 						'<ul class="unstyled-list">';
+		for ($i = 0; $i < count($minority_side); $i++) {
+			$committee_markup .= 						'<li>';
+			$committee_markup .= 							$minority_side[$i]['name'] . ', ' . $minority_side[$i]['state'];
+			if ($minority_side[$i]['rank_in_party'] == 1) {
+				$committee_markup .= 							'&mdash;' . $minority_side[$i]['note'];
 			}
-			$s.= "<li>$firstName $lastName, $state $title</li>";
+			$committee_markup .= 						'</li>';
 		}
-		$s.= "</ul>";
-		$s.= "</div><!-- .bbg-grid -->";
-		$s.= "</section><!-- .usa-grid -->";
+		$committee_markup .= 						'</ul>';
+		$committee_markup .= 					'</div>';
+		$committee_markup .= 				'</div>';
+		$committee_markup .= 			'</div>';
+		$committee_markup .= 		'</li>';
+		$committee_markup .= 	'</ul>';
+		$committee_markup .= '</div>';
 
-
-	$s.= '</div>';
-	$s.= '</li>';
-	$s.= '</ul>';
-	$s.= '</div>';
-
-		return $s;
+		return $committee_markup;
 	}
 
-	// Add shortcode reference to Innovation Series on old posts and pages
+
 	function congressional_committee_shortcode($atts) {
-		return getCongressionalCommittee($atts['id'], $atts['title']);
+		$congress = $atts['congress'];
+		$chamber = $atts['chamber'];
+		$committee_id = $atts['committee-id'];
+		if (!empty($atts['subcommittee-id'])) {
+			$subcommittee_id = $atts['subcommittee-id'];
+		} else {
+			$subcommittee_id = NULL;
+		}
+		$title = $atts['title'];
+		return get_congressional_committee($congress, $chamber, $committee_id, $subcommittee_id, $title);
 	}
 	add_shortcode('congressional_committee', 'congressional_committee_shortcode');
 
