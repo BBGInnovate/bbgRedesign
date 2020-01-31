08b436670de6f1b285ba6b53448f19f0d0c7808e
brianusagm <bsone@usagm.gov>
2020-01-30 17:08:15 -0800

    Fix layout of 404 page.

--- 404.php
+++ 404.php
@@ -10,18 +10,17 @@ require 'inc/bbg-functions-assemble.php';
 get_header();
 ?>
 
-<div class="outer-container" style="margin-bottom: 30px;">
-	<div class="inner-container">
-		<h2><span style="color: #900; font-size: 2em;">404!</span></h2>
-		<h1>Sorry, the page you are looking for doesn’t exist. Just like press freedom in <span id="impact-country" style="color: #900">________</span>.</h1>
-	</div>
-</div>
-
 <main id="main" role="main">
 	<section class="outer-container">
-		<div class="inner-container">
-			<p class="lead-in">Use our search feature for help in finding what you’re looking for.</p>
-			<p>As for press freedom around the world, we’re working on it. Learn how, here: <a href="/news-and-information/threats-to-press/">Threats to Press</a></p>
+		<div class="grid-container">
+			<div class="inner-container">
+				<h2><span style="color: #900; font-size: 2em;">404!</span></h2>
+				<h1>Sorry, the page you are looking for doesn’t exist. Just like press freedom in <span id="impact-country" style="color: #900">________</span>.</h1>
+			</div>
+			<div class="inner-container">
+				<p class="lead-in">Use our search feature for help in finding what you’re looking for.</p>
+				<p>As for press freedom around the world, we’re working on it. Learn how, here: <a href="/news-and-information/threats-to-press/">Threats to Press</a></p>
+			</div>
 		</div>
 	</section>
 </main>
