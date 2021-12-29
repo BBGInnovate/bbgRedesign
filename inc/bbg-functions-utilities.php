bc86336fa0573fbaa29a11c123f7b8e11c023a12
brianusagm <bsone@usagm.gov>
2021-12-28 21:15:33 -0800

    Fix RSS feeds.

--- inc/bbg-functions-utilities.php
+++ inc/bbg-functions-utilities.php
@@ -75,6 +75,7 @@
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_URL,$url);
 		$result=curl_exec($ch);
 		curl_close($ch);
