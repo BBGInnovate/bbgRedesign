4320f6e60d5698bfb4812c477f802eabccb85c33
brianusagm <bsone@usagm.gov>
2024-05-22 00:44:27 -0700

    Change 404 page design.

--- 404.php
+++ 404.php
@@ -15,25 +15,13 @@ get_header();
 		<div class="grid-container">
 			<div class="inner-container">
 				<h2><span style="color: #900; font-size: 2em;">404!</span></h2>
-				<h1>Sorry, the page you are looking for doesn’t exist. Just like press freedom in: <span id="impact-country">________</span></h1>
+				<h1>Sorry, the page you are looking for doesn’t exist.
 			</div>
 			<div class="inner-container">
 				<p class="lead-in">Use our search feature for help in finding what you’re looking for.</p>
-				<p>As for press freedom around the world, we’re working on it. Learn how, here: <a href="/news-and-information/threats-to-press/">Threats to Press</a></p>
 			</div>
 		</div>
 	</section>
 </main>
-
-<script type="text/javascript">
-	var countries = <?php echo json_encode(getThreatsToPressCountries()); ?>;
-	var countriesCount = countries.length;
-	setInterval(
-		function() {
-			var rand = Math.floor(Math.random() * countriesCount);
-			document.getElementById('impact-country').innerHTML = countries[rand];
-		},
-	2000);
-</script>
 
 <?php get_footer(); ?>
